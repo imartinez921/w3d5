d73c99079bf5fe2a1b675c1776ee1e9d286bf680
App Academy Student <email@email.com>
2022-06-10 16:08:24 -0400

    polynode classes complete

--- skeleton 2/lib/00_tree_node.rb
+++ skeleton 2/lib/00_tree_node.rb
@@ -1,3 +1,40 @@
 class PolyTreeNode
 
+  attr_reader :parent, :children, :value
+
+  def initialize(value = nil)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(p_node)
+    if self.parent != p_node && self.parent != nil
+      self.parent.children.delete(self)
+    end
+    @parent = p_node
+    if self.parent != nil && !p_node.children.include?(self)
+      p_node.children << self
+    end
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    raise "Not my child!" if !self.children.include?(child_node)
+    child_node.parent = nil
+  end
+
+  def dfs(target)
+    return self if self.value == target
+    return nil if self.children.empty?
+
+    self.children.each do |child| 
+      
+      return child.dfs(target)
+    end
+  end 
+
 end
